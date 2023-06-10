3eb6bacee58594b400bb66368221405e8c5420e7
IdrisFallout <waithakasam2017@gmail.com>
2023-06-10 11:36:22 +0300

    add socketio

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello Microsoft Azure!'
+    return 'Hello Microsoft Azure Room2!'
 
 
 if __name__ == '__main__':
