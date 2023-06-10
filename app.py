6eb28c8c4722facb81ee1615c17904a475e2c377
IdrisFallout <waithakasam2017@gmail.com>
2023-06-10 11:51:40 +0300

    add actual code

--- app.py
+++ app.py
@@ -6,9 +6,28 @@ app.secret_key = 'random secret key!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 
-# @app.route('/')
-# def hello_world():  # put application's code here
-#     return 'Hello Microsoft Azure update 4!'
+@socketio.on('join')
+def join(message):
+    username = message['username']
+    room = message['room']
+    join_room(room)
+    print('RoomEvent: {} has joined the room {}\n'.format(username, room))
+    emit('ready', {username: username}, to=room, skip_sid=request.sid)
+
+
+@socketio.on('data')
+def transfer_data(message):
+    username = message['username']
+    room = message['room']
+    data = message['data']
+    print('DataEvent: {} has sent the data:\n {}\n'.format(username, data))
+    emit('data', data, to=room, skip_sid=request.sid)
+
+
+@socketio.on_error_default
+def default_error_handler(e):
+    print("Error: {}".format(e))
+    socketio.stop()
 
 
 if __name__ == '__main__':
