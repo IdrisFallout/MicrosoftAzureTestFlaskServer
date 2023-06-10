f0862c318826dd5c85aae8b17e0182eacb8fe45c
IdrisFallout <waithakasam2017@gmail.com>
2023-06-10 11:41:07 +0300

    add app secret key

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ from flask import Flask, request
 from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
+app.secret_key = 'random secret key!'
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello Microsoft Azure Room2!'
+    return 'Hello Microsoft Azure update 3!'
 
 
 if __name__ == '__main__':
